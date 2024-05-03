d5a6e4173d137ff51f90123f6ae03802d989f96e
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-05-03 18:48:29 +0300

    Create button to rotate -Y

---
+++ PPUViewer.h
@@ -0,0 +1,34 @@
+//
+// copyright 2018 Palestine Polytechnic Univeristy
+//
+// This software can be used and/or modified for academich use as long as 
+// this commented part is listed
+//
+// Last modified by: Zein Salah, on 23.02.2024
+//
+
+
+#pragma once
+
+#include <QWidget>
+#include <QPushButton>
+
+class RenderWidget;
+
+class PPUViewer : public QWidget
+{
+  Q_OBJECT
+
+  public:
+    PPUViewer();
+
+  private:
+    RenderWidget *m_RenderWidget;
+    QPushButton* m_ZoomInButton;
+    QPushButton* m_ZoomOutButton;
+    QPushButton* m_RotateButton;
+    QPushButton* m_RotateYMButton;
+
+
+};
+
